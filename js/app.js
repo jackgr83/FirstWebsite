4e372ca709846effa32e9b3d19d09e1a053c0ecb
jackgr83 <jackgr83@gmail.com>
2020-04-04 20:59:43 -0400

    Added panel and content

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 
+
+var project = document.getElementsByClassName('projects');
+
+for (var i = 0; i < project.length; i++) {
+  project[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+
+  });
+}
