b6a9975de7da6a3d078423fe2d038b256666eba7
jackgr83 <jackgr83@gmail.com>
2020-04-05 18:05:18 -0400

    Added internships to panel

--- js/app.js
+++ js/app.js
@@ -1,16 +1,43 @@
+//reference dom elements
+var domElements = {
+  "oads": document.getElementById('oads'),
+  "project": document.getElementsByClassName('projects'),
+  "main": document.getElementById('main'),
+  "subheader": document.getElementById('subheader'),
+  "lynchburg": document.getElementById('lynchburg'),
+  "CBF": document.getElementById('CBF')
+}
 
+// setup button handlers
+for (var i = 0; i < domElements.project.length; i++) {
+  domElements.project[i].addEventListener("click", toggleMenu);
+}
+domElements.oads.addEventListener("click", writeOADsHTML);
+domElements.CBF.addEventListener("click", embedCBF);
 
-var project = document.getElementsByClassName('projects');
+//
+//supporting functions
+//
 
-for (var i = 0; i < project.length; i++) {
-  project[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
+function toggleMenu() {
+  this.classList.toggle("active");
+  var content = this.nextElementSibling;
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  }
 
-  });
+};
+
+function writeOADsHTML() {
+  domElements.main.innerHTML = "<p><h3>OADs Portal</h3><p>Click the button below to be directed to the OADs portal.</p><button type='button' name='OADs'>OADS Portal</button>"
+  if (domElements.subheader.innerHTML.indexOf("Home") == -1 ) {
+    domElements.subheader.innerHTML = "<a href='index.html'>Home</a> |" + subheader.innerHTML;
+  } else {}
+
+};
+
+function embedCBF () {
+  domElements.main.innerHTML += '<embed src="images/CBF.pdf" type="application/pdf" width="100%" height="600px" />'
 }
