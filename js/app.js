3b2a62c1ce54626f106986a6284c416424df47f9
jackgr83 <jackgr83@gmail.com>
2020-04-05 20:01:56 -0400

    added landing pages for internships. open links in new tab

--- js/app.js
+++ js/app.js
@@ -12,12 +12,18 @@ var domElements = {
 for (var i = 0; i < domElements.project.length; i++) {
   domElements.project[i].addEventListener("click", toggleMenu);
 }
-domElements.oads.addEventListener("click", writeOADsHTML);
-domElements.CBF.addEventListener("click", embedCBF);
+domElements.oads.addEventListener("click", writeOADs);
+domElements.CBF.addEventListener("click", writeCBF);
+domElements.lynchburg.addEventListener("click", writeLynchburg)
 
 //
 //supporting functions
 //
+function writeHomeBtn() {
+  if (domElements.subheader.innerHTML.indexOf("Home") == -1 ) {
+    domElements.subheader.innerHTML = "<a href='index.html'>Home</a> |" + subheader.innerHTML;
+  } else {}
+}
 
 function toggleMenu() {
   this.classList.toggle("active");
@@ -30,14 +36,17 @@ function toggleMenu() {
 
 };
 
-function writeOADsHTML() {
+function writeOADs() {
   domElements.main.innerHTML = "<p><h3>OADs Portal</h3><p>Click the button below to be directed to the OADs portal.</p><button type='button' name='OADs'>OADS Portal</button>"
-  if (domElements.subheader.innerHTML.indexOf("Home") == -1 ) {
-    domElements.subheader.innerHTML = "<a href='index.html'>Home</a> |" + subheader.innerHTML;
-  } else {}
-
+  writeHomeBtn();
 };
 
-function embedCBF () {
-  domElements.main.innerHTML += '<embed src="images/CBF.pdf" type="application/pdf" width="100%" height="600px" />'
+function writeCBF() {
+  domElements.main.innerHTML = '<p><h3>Chesapeake Bay Foundation</h3><p>As part of my internship with the Chesapeake Bay Foundation, I utilized GIS technologies and data analysis techniques to assess tree canopy cover in the City of Hopewell. My assessment below allowed CBF to achieve $200,000 of grant funding to plant trees in Hopewell, Virginia. <p><embed src="images/CBF.pdf" type="application/pdf" width="100%" height="600px" />'
+  writeHomeBtn();
+};
+
+function writeLynchburg() {
+  domElements.main.innerHTML = "<p><h3>City of Lynchburg</h3><p>While interning with the City of Lynchburg's GIS Office within the city's IT Department, I organized the city's stormwater and public works databases among other things. Check out everything else I did during my internship by clicking the link below<p>"
+  domElements.main.innerHTML += "<a href='https://arcg.is/1uD5Pu' target='_blank'>StoryMap</a>"
 }
